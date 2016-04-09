dd2ad7736b1d3c25b4e0e6771a128a00904f2cb3
andrew-luhring <andrew.luhring@gmail.com>
2016-04-09 14:51:07 -0500

    remove global this

--- article-time-calculator.js
+++ article-time-calculator.js
@@ -1,54 +1,50 @@
-var root = this;
-
 (function(){
-'use strict';
-function getElement(selector){
-  return document.querySelector(selector);
-}
+  'use strict';
+  function getElement(selector){
+    return document.querySelector(selector);
+  }
 
-function setTimeProperties(textLength){
-  this.minutesToRead = textLength / this.wordsPerMinute;
-  this.formattedTime = this.formatTimeFn(this.minutesToRead);
-}
+  function setTimeProperties(textLength){
+    this.minutesToRead = textLength / this.wordsPerMinute;
+    this.formattedTime = this.formatTimeFn(this.minutesToRead);
+  }
 
-function pluralizeFn(prop, amount){
-  return (amount > 1 || amount === 0) ? prop + 's' : prop;
-}
+  function pluralizeFn(prop, amount){
+    return (amount > 1 || amount === 0) ? prop + 's' : prop;
+  }
+  
+  class ArticleReader{
+    constructor(configObj){
+      let _configObj = (typeof configObj === 'object' && configObj !== null && !Array.isArray(configObj)) ? configObj : {};
+      //average wpm is 200
+      this.wordsPerMinute = _configObj.wordsPerMinute || 200;
+      this.nameForMinute = _configObj.nameForMinute || 'minute';
+      this.nameForSecond = _configObj.nameForSecond || 'second';
+      this.pluralizeInterceptor = _configObj.nameForSecond || pluralizeFn;
 
+      this.formatTimeFn = _configObj.formatTime || function(timeInMinutes){
+        let minutes = Math.floor(timeInMinutes)
+        , secondsAsPercentOfMinute = Math.round((timeInMinutes % minutes) * 100) / 100
+        , seconds = (isNaN(secondsAsPercentOfMinute)) ? 0 : secondsAsPercentOfMinute * 60
+        , minuteWord = this.pluralizeInterceptor(this.nameForMinute, minutes)
+        , secondWord = this.pluralizeInterceptor(this.nameForSecond, seconds);
 
-class ArticleReader{
-  constructor(configObj){
-    let _configObj = (typeof configObj === 'object' && configObj !== null && !Array.isArray(configObj)) ? configObj : {};
-    //average wpm is 200
-    this.wordsPerMinute = _configObj.wordsPerMinute || 200;
-    this.nameForMinute = _configObj.nameForMinute || 'minute';
-    this.nameForSecond = _configObj.nameForSecond || 'second';
-    this.pluralizeInterceptor = _configObj.nameForSecond || pluralizeFn;
+        return minutes + ' ' + minuteWord + ' ' + seconds + ' ' + secondWord;
+      }
+    }
 
-    this.formatTimeFn = _configObj.formatTime || function(timeInMinutes){
-      let minutes = Math.floor(timeInMinutes)
-      , secondsAsPercentOfMinute = Math.round((timeInMinutes % minutes) * 100) / 100
-      , seconds = (isNaN(secondsAsPercentOfMinute)) ? 0 : secondsAsPercentOfMinute * 60
-      , minuteWord = this.pluralizeInterceptor(this.nameForMinute, minutes)
-      , secondWord = this.pluralizeInterceptor(this.nameForSecond, seconds);
+    getLengthBasedOnWordCount(num){
+      setTimeProperties.call(this, num);
+      return this.formattedTime;
+    }
 
-      return minutes + ' ' + minuteWord + ' ' + seconds + ' ' + secondWord;
+    getLengthBasedOnElement(selector){
+      let elem = getElement(selector);
+      if(!elem){
+        throw new Error('there was no element with the selector: ' + selector);
+      }
+      let text = elem.textContent.split(' ');
+      return this.getLengthBasedOnWordCount(text.length);
     }
   }
-
-  getLengthBasedOnWordCount(num){
-    setTimeProperties.call(this, num);
-    return this.formattedTime;
-  }
-
-  getLengthBasedOnElement(selector){
-    let elem = getElement(selector);
-    if(!elem){
-      throw new Error('there was no element with the selector: ' + selector);
-    }
-    let text = elem.textContent.split(' ');
-    return this.getLengthBasedOnWordCount(text.length);
-  }
-}
-
 })();
