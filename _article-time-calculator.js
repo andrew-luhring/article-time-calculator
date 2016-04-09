559e7d47674dd3dcdaee7317ac51c085d735e430
andrew-luhring <andrew.luhring@gmail.com>
2016-04-09 14:48:15 -0500

    first commit

---
+++ _article-time-calculator.js
@@ -0,0 +1,39 @@
+
+
+//var instance = new ArticleReader();
+
+//function constructDomTest(id, amt){
+//  function populateElemWithNumberOfWords(elem, num){
+//    var str = '';
+//    let i = 0;
+//
+//    for(; i < num; i++){
+//      str += ' derp';
+//    }
+//    elem.textContent = str;
+//  }
+//  let dom = document.body;
+//  let testP = document.createElement('p');
+//  testP.id = id;
+//  populateElemWithNumberOfWords(testP, amt);
+//  dom.appendChild(testP);
+//}
+//constructDomTest('testparagraph', 200);
+//console.log(instance.getLengthBasedOnElement('#testparagraph'));
+//console.assert(instance.getLengthBasedOnElement('#testparagraph') === "1 minute 0 seconds");
+//constructDomTest('otherp', 500);
+//console.log(instance.getLengthBasedOnElement('#otherp'));
+//console.assert(instance.getLengthBasedOnElement('#otherp') === "2 minutes 30 seconds");
+//constructDomTest('anotherp', 550);
+//console.log(instance.getLengthBasedOnElement('#anotherp'));
+//console.assert(instance.getLengthBasedOnElement('#anotherp') === "2 minutes 45 seconds");
+//console.log(instance.getLengthBasedOnElement('#fail'));
+//
+//console.log(instance.getLengthBasedOnWordCount(200));
+//console.log(instance.getLengthBasedOnWordCount(300));
+//console.log(instance.getLengthBasedOnWordCount(400));
+//console.log(instance.getLengthBasedOnWordCount(0));
+//console.assert(instance.getLengthBasedOnWordCount(200) === "1 minute 0 seconds");
+//console.assert(instance.getLengthBasedOnWordCount(300) === "1 minute 30 seconds");
+//console.assert(instance.getLengthBasedOnWordCount(400) === "2 minutes 0 seconds");
+//console.assert(instance.getLengthBasedOnWordCount(0) === "0 minutes 0 seconds");
---
+++ article-time-calculator.js
@@ -0,0 +1,54 @@
+var root = this;
+
+(function(){
+'use strict';
+function getElement(selector){
+  return document.querySelector(selector);
+}
+
+function setTimeProperties(textLength){
+  this.minutesToRead = textLength / this.wordsPerMinute;
+  this.formattedTime = this.formatTimeFn(this.minutesToRead);
+}
+
+function pluralizeFn(prop, amount){
+  return (amount > 1 || amount === 0) ? prop + 's' : prop;
+}
+
+
+class ArticleReader{
+  constructor(configObj){
+    let _configObj = (typeof configObj === 'object' && configObj !== null && !Array.isArray(configObj)) ? configObj : {};
+    //average wpm is 200
+    this.wordsPerMinute = _configObj.wordsPerMinute || 200;
+    this.nameForMinute = _configObj.nameForMinute || 'minute';
+    this.nameForSecond = _configObj.nameForSecond || 'second';
+    this.pluralizeInterceptor = _configObj.nameForSecond || pluralizeFn;
+
+    this.formatTimeFn = _configObj.formatTime || function(timeInMinutes){
+      let minutes = Math.floor(timeInMinutes)
+      , secondsAsPercentOfMinute = Math.round((timeInMinutes % minutes) * 100) / 100
+      , seconds = (isNaN(secondsAsPercentOfMinute)) ? 0 : secondsAsPercentOfMinute * 60
+      , minuteWord = this.pluralizeInterceptor(this.nameForMinute, minutes)
+      , secondWord = this.pluralizeInterceptor(this.nameForSecond, seconds);
+
+      return minutes + ' ' + minuteWord + ' ' + seconds + ' ' + secondWord;
+    }
+  }
+
+  getLengthBasedOnWordCount(num){
+    setTimeProperties.call(this, num);
+    return this.formattedTime;
+  }
+
+  getLengthBasedOnElement(selector){
+    let elem = getElement(selector);
+    if(!elem){
+      throw new Error('there was no element with the selector: ' + selector);
+    }
+    let text = elem.textContent.split(' ');
+    return this.getLengthBasedOnWordCount(text.length);
+  }
+}
+
+})();
