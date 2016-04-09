57c4cfb4c63afebf4f0cce2c2c7a8bc32ba556ec
andrew-luhring <andrew.luhring@gmail.com>
2016-04-09 15:04:02 -0500

    adds lodash's exporting function process

--- article-time-calculator.js
+++ article-time-calculator.js
@@ -1,3 +1,4 @@
+var root = this;
 (function(){
   'use strict';
   function getElement(selector){
@@ -12,7 +13,7 @@
   function pluralizeFn(prop, amount){
     return (amount > 1 || amount === 0) ? prop + 's' : prop;
   }
-  
+
   class ArticleReader{
     constructor(configObj){
       let _configObj = (typeof configObj === 'object' && configObj !== null && !Array.isArray(configObj)) ? configObj : {};
@@ -38,7 +39,7 @@
       return this.formattedTime;
     }
 
-    getLengthBasedOnElement(selector){
+    getLengthBasedOnSelector(selector){
       let elem = getElement(selector);
       if(!elem){
         throw new Error('there was no element with the selector: ' + selector);
@@ -46,5 +47,48 @@
       let text = elem.textContent.split(' ');
       return this.getLengthBasedOnWordCount(text.length);
     }
+
+    getLengthBasedOnElement(elem){
+      let text = elem.textContent.split(' ');
+      return this.getLengthBasedOnWordCount(text.length);
+    }
+
+  }
+
+  /**
+   * NOTE: Everything after this (the whole exporting thing) is taken and slightly modified from lodash (https://github.com/lodash/lodash).
+   */
+  var objectTypes = {
+    'function': true,
+    'object': true
+  };
+
+  var freeExports = (objectTypes[typeof exports] && exports && !exports.nodeType) ? exports : undefined;
+
+  /** Detect free variable `module`. */
+  var freeModule = (objectTypes[typeof module] && module && !module.nodeType) ? module : undefined;
+
+  /** Detect the popular CommonJS extension `module.exports`. */
+  var moduleExports = (freeModule && freeModule.exports === freeExports) ? freeExports : undefined;
+
+
+// Some AMD build optimizers like r.js check for condition patterns like the following:
+  if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
+    define(function() {
+      return ArticleReader;
+    });
+  }
+// Check for `exports` after `define` in case a build optimizer adds an `exports` object.
+  else if (freeExports && freeModule) {
+    // Export for Node.js.
+    if (moduleExports) {
+      freeModule.exports = ArticleReader;
+    }
+    // Export for CommonJS support.
+    freeExports.ArticleReader = ArticleReader;
+  }
+  else {
+    // Export to the global object.
+    root.ArticleReader = ArticleReader;
   }
 })();
